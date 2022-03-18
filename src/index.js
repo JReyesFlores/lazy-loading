731bb50d2cc970d0bf594f6dcd77cd34c11c6a09
jhon.phileppe.reyes.flores <jhon.reyes@openserver.com.pe>
2022-03-18 14:32:05 -0500

    Finalizando las clase de Manipulación del DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from './lazy';
 
-console.log('Happy hacking :)')
+const images = document.querySelector('#images');
+const addButton = document.querySelector('button');
+const [min, max] = [1, 122];
+
+const random = () => Math.floor(Math.random() * (max - min));
+
+const createImageNodes = () => {
+  const container = document.createElement('div');
+  container.className = 'p-4';
+
+  const foxImage = document.createElement('img');
+  foxImage.className = 'mx-auto';
+  foxImage.width = 320;
+  // foxImage.src = `https://randomfox.ca/images/${random()}.jpg`;
+  foxImage.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+  container.appendChild(foxImage);
+
+  return container;
+};
+
+const addImage = () => {
+  const newImage = createImageNodes();
+  images.append(newImage);
+  registerImage(newImage);
+};
+
+//const nuevaImagen = createImageNodes();
+//images.appendChild(nuevaImagen);
+//images.append(nuevaImagen);
+
+addButton.addEventListener('click', addImage);
+
+/**
+ * Implementaciones pendientes (RETO PLATZI)
+ * 1. Agregar un recuadro gris antes de las imagens
+ * 2. Agregar un boton para limpiar las imagenes
+ * 3. Reporte en la consolo de imagenes cargadas e imagenes agregadas
+ */
