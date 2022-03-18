23965503366771954b83f7e8f33df35d5ce7182e
JReyesFlores <JReyesFlores@users.noreply.github.com>
2022-03-18 19:36:41 +0000

    deploy: e550da244f266fc319e731c69d0df4d1dcd703e8

---
+++ _dist_/lazy.js
@@ -0,0 +1,24 @@
+const isIntersecting = (entry) => {
+  //se pueden validar cosas adicionales
+  return entry.isIntersecting;
+};
+
+const accion = (entry) => {
+  const container = entry.target;
+  const imagen = container.querySelector('img');
+  const url = imagen.dataset.src;
+  imagen.src = url;
+  //console.log(url);
+
+  // des registra la imagen
+  observer.unobserve(container);
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.filter(isIntersecting).forEach(accion);
+});
+
+export const registerImage = (Image) => {
+  // Registra la webAPI de IntersectionObserver
+  observer.observe(Image);
+};
